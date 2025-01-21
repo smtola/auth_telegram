3cc7a238357fe7805cc2787afeac53c27508d391
somtola <somtola.smt@gmail.com>
2025-01-21 14:55:11 +0700

    new update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,17 +20,6 @@ const LoginPage: React.FC = () => {
   const handleTelegramAuth = async (user: TelegramUser) => {
     console.log("Telegram User Authenticated:", user);
     setUser(user);
-   const getUpdateUrl = `https://api.telegram.org/bot7786727966:AAENBDXFKdVcYAPYkKFkpEta2-UlvoyB1q0/getUpdates`;
-        const updateResponse = await fetch(getUpdateUrl);
-        const updateData = await updateResponse.json();
-
-    // Check if the latest message is "/start"
-    const latestUpdate = updateData.result[updateData.result.length + 1];
-      const messageText = latestUpdate?.message?.text;
-    if (!messageText) {
-      setServerMessage("No message detected.");
-      return;
-    }
 
    try {
     const response = await fetch("/api/notify-user", {
@@ -43,7 +32,7 @@ const LoginPage: React.FC = () => {
       last_name: user.last_name || "",
       username: user.username || "",
     },
-      message: messageText,
+      message: '/start'
     }),
   });
 
